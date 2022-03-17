40ad1d549b425a32eda36e45e2d0a0750dd3d16b
viswareddy17 <baddipalliviswa17@gmail.com>
2022-03-17 02:55:53 +0000

    Dbconnect.java

--- Dbconnect.java
+++ Dbconnect.java
@@ -1,2 +1,3 @@
 hi this is the file to connect to DB
-updating the code at working area itself`
+updating the code at working area itself
+withoutadd command , i have to commit directly to local repository after modifing changes
